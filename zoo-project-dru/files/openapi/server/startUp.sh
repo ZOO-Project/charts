c0cf50baa647a7bc18d8f3e47c8edc2edf246d90
gfenoy <gerald.fenoy@geolabs.fr>
2023-09-12 19:13:23 +0200

    Run ZOO-FPM as www-data, use new DRU docker image
    Update the helm chart and app version

--- zoo-project-dru/files/openapi/server/startUp.sh
+++ zoo-project-dru/files/openapi/server/startUp.sh
@@ -20,6 +20,8 @@ echo "START FPM in 5 seconds"
 sleep 5
 
 cd /usr/lib/cgi-bin
-./zoo_loader_fpm ./main.cfg 2> /var/log/zoofpm.log >> /var/log/zoofpm.log
+touch /var/log/zoofpm.log
+chown www-data:www-data /var/log/zoofpm.log
+su www-data -s /bin/bash -c "./zoo_loader_fpm ./main.cfg 2> /var/log/zoofpm.log >> /var/log/zoofpm.log"
 
 echo "STARTING FPM DONE"
