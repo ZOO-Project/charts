91fa847abc37a98f2d2571875e3d26dbac384fba
Gérald Fenoy <gerald.fenoy@geolabs.fr>
2025-02-10 18:00:13 +0100

    Bump chat version including the fix for security_service.py

--- zoo-project-dru/files/zoo-project/security_service.py
+++ zoo-project-dru/files/zoo-project/security_service.py
@@ -70,9 +70,12 @@ def securityIn(conf, inputs, outputs):
         if "servicesNamespace" in conf and "debug" in conf["servicesNamespace"]:
             zoo.debug(f"No JWT service available: {str(e)}")
     rPath = conf["servicesNamespace"]["path"] + "/"
+    has_rpath=False
     for i in conf["renv"]:
         if i.count("SERVICES_NAMESPACE"):
-            rPath += conf["renv"][i]
+            if not(has_rpath):
+                rPath += conf["renv"][i]
+                has_rpath=True
             if "auth_env" not in conf:
                 conf["auth_env"] = {"user": conf["renv"][i], "cwd": rPath}
             else:
@@ -96,7 +99,7 @@ def securityIn(conf, inputs, outputs):
         if "required_files" in conf["servicesNamespace"]:
             rFiles = conf["servicesNamespace"]["required_files"].split(",")
             for i in range(len(rFiles)):
-                zoo.info(f"Copy file {rFile[i]}")
+                zoo.info(f"Copy file {rFiles[i]}")
                 shutil.copyfile(
                     conf["renv"]["CONTEXT_DOCUMENT_ROOT"] + "/" + rFiles[i],
                     rPath + "/" + rFiles[i],
